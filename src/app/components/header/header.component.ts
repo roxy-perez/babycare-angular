1d9b81943df0f55a4d1edba81ab340c0dd984aa9
RoxyWTF <necropsia32@gmail.com>
2023-03-14 23:52:05 +0000

    Creating the Register page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 import {
   faGear,
 } from '@fortawesome/free-solid-svg-icons';
@@ -10,4 +11,13 @@ import {
 })
 export class HeaderComponent {
   faGear = faGear;
+
+  constructor(public router: Router) { }
+
+  show(): boolean {
+    if (this.router.url !== '/register' && this.router.url !== '/login') {
+      return true;
+    }
+    return false;
+  }
 }
